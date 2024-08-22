9ec629b671311c2452da3ee72529d539933d98ac
Louanne Dronne <louanne.dronne@gouv.nc>
2024-08-22 15:23:33 +1100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AthletismeComponent } from './athletisme/athletisme.component';
+import { GameComponent } from './game/game.component';
+import { LigueComponent } from './ligue/ligue.component';
+import { RgpdComponent } from './rgpd/rgpd.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent},
+  { path: 'athletisme', component: AthletismeComponent},
+  { path: 'game', component: GameComponent },
+  { path: 'ligue', component: LigueComponent },
+  { path: 'rgpd', component: RgpdComponent },
+  // Ajoutez d'autres routes ici
+];
