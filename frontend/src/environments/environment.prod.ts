f9afbb455da80fe6417c5fef808c61576eb43bb3
3234ROU <cyril.rousteau@opt.nc>
2024-10-10 11:46:44 +1100

    Suppression des fichiers d'environnement de l'index de Git

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true, 
-    emailjs: {
-      publicKey: 'EMj9AnAU3gidc6MMJ',
-      gmailServiceId: 'service_8wcoqok',
-      templateId: 'template_tgzc0t8'
-      }
-  };
--- frontend/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false, // true dans environment.prod.ts
-    emailjs: {
-        publicKey: 'EMj9AnAU3gidc6MMJ',  // Remplace directement par les valeurs
-        gmailServiceId: 'service_8wcoqok',
-        templateId: 'template_tgzc0t8'
-    }
-  };
